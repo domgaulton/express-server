683171909ac5b9c00e0f286a7d8b6c916d5dff0c
Dom Gaulton <d.gaulton@catchdigital.com>
2019-07-23 11:01:08 +0100

    revisited server and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 ### Express Server basics
+
+- Run `npm install` to install dependencies
+- Terminal `$ node server.js` then point browser to `http://localhost:3000/` to see returned message or `http://localhost:3000/profile` to see returned object
--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const express = require('express');
+const express = require("express");
 
 // See documentation https://expressjs.com/en/starter/installing.html
 const app = express();
 
 // with express request and response are shortened
-app.get('/', (req, res) => {
-  const user = {
-    name: 'Sally',
-    age: 30
-  }
-  // able to send header type JSON very easily
-  res.send(user);
+app.get("/", (req, res) => {
+  res.send("home page message");
 });
 
 // set a URL
-app.get('/profile', (req, res) => {
-  res.send('profile page');
+app.get("/profile", (req, res) => {
+  const user = {
+    name: "Sally",
+    age: 30
+  };
+  // able to send header type JSON very easily
+  res.send(user);
 });
 
 app.listen(3000);
